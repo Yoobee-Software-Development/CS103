4dfd2d7b63481eaf80298a2708fed01de854ea94
McCreepy <mccreepyy@hotmail.com>
2023-06-14 16:52:38 +1200

    Added Menu Logo and Prices for Food

--- CS103.cpp
+++ CS103.cpp
@@ -1,1 +1,35 @@
-// CS103 AS2
+// Zach Lovett & James McGregor CS103 Project
+#include <iostream>
+
+// Menu Logo
+void drawMenu() {
+    std::cout << "   ********           ********   *****************   *******       *****   *****    ***** \n";
+    std::cout << "   *   *   *         *   *   *   *               *   *      *      *   *   *   *    *   * \n";
+    std::cout << "   *   **   *       *   **   *   *   *************   *       *     *   *   *   *    *   * \n";
+    std::cout << "   *   * *   *     *   * *   *   *   *               *   **   *    *   *   *   *    *   * \n";
+    std::cout << "   *   *  *   *   *   *  *   *   *   *************   *   * *   *   *   *   *   *    *   * \n";
+    std::cout << "   *   *   *   * *   *   *   *   *   *           *   *   *  *   *  *   *   *   *    *   * \n"; 
+    std::cout << "   *   *    *   *   *    *   *   *   *************   *   *   *   * *   *   *   *    *   * \n";
+    std::cout << "   *   *     *     *     *   *   *   *               *   *    *   **   *   *   ******   * \n";
+    std::cout << "   *   *      *   *      *   *   *   *************   *   *     *       *   *            * \n";
+    std::cout << "   *   *       * *       *   *   *               *   *   *      *      *   *            * \n";
+    std::cout << "   *****        *        *****   *****************   *****       *******   ************** \n";
+}
+
+// Student & Admin Login
+
+// Menu Details
+int steakcheesepie = 5.0;
+int hotchips = 10.0;
+int pizza = 12.50;
+int sushi = 15.0;
+int sausageroll = 5.0;
+int brownie = 7.50;
+int sandwich = 5.0;
+int bagel = 5.0;
+int donut = 7.0;
+// Payment
+
+// Discounts
+
+// Billing
